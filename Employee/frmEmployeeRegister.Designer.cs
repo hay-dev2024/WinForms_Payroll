3509a654b250f81d523edfc67895942867c8d026
hay <hay.dev2024@gmail.com>
2025-12-22 11:57:28 +0900

    feat: add keydown events in employee form

--- Employee/frmEmployeeRegister.Designer.cs
+++ Employee/frmEmployeeRegister.Designer.cs
@@ -28,20 +28,380 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmpId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTin = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBankDetails = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.dtpDob = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgEmpId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgMobile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgTinNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgDob = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgBankDetails = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(198, 196);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(22, 216);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(63, 12);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "File Name";
+            this.lblFileName.Visible = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(22, 231);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Emp ID :";
+            // 
+            // txtEmpId
+            // 
+            this.txtEmpId.Location = new System.Drawing.Point(310, 37);
+            this.txtEmpId.Name = "txtEmpId";
+            this.txtEmpId.ReadOnly = true;
+            this.txtEmpId.Size = new System.Drawing.Size(210, 21);
+            this.txtEmpId.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mobile :";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(310, 64);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(210, 21);
+            this.txtMobile.TabIndex = 4;
+            this.txtMobile.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMobile_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "TIN No. :";
+            // 
+            // txtTin
+            // 
+            this.txtTin.Location = new System.Drawing.Point(310, 91);
+            this.txtTin.Name = "txtTin";
+            this.txtTin.Size = new System.Drawing.Size(210, 21);
+            this.txtTin.TabIndex = 4;
+            this.txtTin.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtTin_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Bank Details :";
+            // 
+            // txtBankDetails
+            // 
+            this.txtBankDetails.Location = new System.Drawing.Point(310, 118);
+            this.txtBankDetails.Multiline = true;
+            this.txtBankDetails.Name = "txtBankDetails";
+            this.txtBankDetails.Size = new System.Drawing.Size(210, 87);
+            this.txtBankDetails.TabIndex = 4;
+            this.txtBankDetails.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBankDetails_KeyDown);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(103, 231);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(574, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Name :";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(634, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(210, 21);
+            this.txtName.TabIndex = 4;
+            this.txtName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtName_KeyDown);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(576, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 12);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Email :";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(634, 67);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(210, 21);
+            this.txtEmail.TabIndex = 4;
+            this.txtEmail.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEmail_KeyDown);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(583, 97);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 12);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "DOB :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(561, 124);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 12);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Address :";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(634, 121);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(210, 87);
+            this.txtAddress.TabIndex = 4;
+            this.txtAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddress_KeyDown);
+            // 
+            // dtpDob
+            // 
+            this.dtpDob.CustomFormat = "dd/MM/yyyy";
+            this.dtpDob.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDob.Location = new System.Drawing.Point(634, 94);
+            this.dtpDob.Name = "dtpDob";
+            this.dtpDob.Size = new System.Drawing.Size(210, 21);
+            this.dtpDob.TabIndex = 5;
+            this.dtpDob.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dtpDob_KeyDown);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(465, 231);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(546, 231);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(627, 231);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgEmpId,
+            this.dgName,
+            this.dgMobile,
+            this.dgEmail,
+            this.dgTinNo,
+            this.dgDob,
+            this.dgBankDetails,
+            this.dgAddress});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 275);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(866, 274);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // dgEmpId
+            // 
+            this.dgEmpId.HeaderText = "Emp Id";
+            this.dgEmpId.Name = "dgEmpId";
+            // 
+            // dgName
+            // 
+            this.dgName.HeaderText = "Name";
+            this.dgName.Name = "dgName";
+            // 
+            // dgMobile
+            // 
+            this.dgMobile.HeaderText = "Mobile";
+            this.dgMobile.Name = "dgMobile";
+            // 
+            // dgEmail
+            // 
+            this.dgEmail.HeaderText = "Email";
+            this.dgEmail.Name = "dgEmail";
+            // 
+            // dgTinNo
+            // 
+            this.dgTinNo.HeaderText = "TIN No.";
+            this.dgTinNo.Name = "dgTinNo";
+            // 
+            // dgDob
+            // 
+            this.dgDob.HeaderText = "Dob";
+            this.dgDob.Name = "dgDob";
+            // 
+            // dgBankDetails
+            // 
+            this.dgBankDetails.HeaderText = "Bank Details";
+            this.dgBankDetails.Name = "dgBankDetails";
+            // 
+            // dgAddress
+            // 
+            this.dgAddress.HeaderText = "Address";
+            this.dgAddress.Name = "dgAddress";
             // 
             // frmEmployeeRegister
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1210, 691);
+            this.ClientSize = new System.Drawing.Size(891, 561);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtpDob);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtBankDetails);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtTin);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmpId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.pictureBox);
             this.Name = "frmEmployeeRegister";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Employee Register";
+            this.Load += new System.EventHandler(this.frmEmployeeRegister_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmpId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBankDetails;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.DateTimePicker dtpDob;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgEmpId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgMobile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgTinNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgDob;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgBankDetails;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgAddress;
     }
 }
--- Employee/frmEmployeeRegister.cs
+++ Employee/frmEmployeeRegister.cs
@@ -12,9 +12,132 @@ namespace Payroll.Employee
 {
     public partial class frmEmployeeRegister : Form
     {
+        string fileName;
+
         public frmEmployeeRegister()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true, Multiselect = false })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = ofd.FileName;
+                    lblFileName.Text = fileName;
+                    pictureBox.Image = Image.FromFile(fileName);
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = null;
+        }
+
+        private void frmEmployeeRegister_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtName;
+        }
+
+        private void txtName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(txtName.Text.Length > 0)
+                {
+                    txtMobile.Focus();
+                }
+                else
+                {
+                    txtName.Focus();
+                }
+            }
+        }
+
+        private void txtMobile_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(txtMobile.Text.Length > 0)
+                {
+                    txtEmail.Focus();
+                }
+                else
+                {
+                    txtMobile.Focus();
+                }
+            }
+        }
+
+        private void txtEmail_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(txtEmail.Text.Length > 0)
+                {
+                    txtTin.Focus();
+                }
+                else
+                {
+                    txtEmail.Focus();
+                }
+            }
+        }
+
+        private void txtTin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(txtTin.Text.Length > 0)
+                {
+                    dtpDob.Focus();
+                }
+                else
+                {
+                    txtTin.Focus();
+                }
+            }
+        }
+
+        private void dtpDob_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                txtBankDetails.Focus();
+            }
+        }
+
+        private void txtBankDetails_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(txtBankDetails.Text.Length > 0)
+                {
+                    txtAddress.Focus();
+                }
+                else
+                {
+                    txtBankDetails.Focus();
+                }
+            }
+        }
+
+        private void txtAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(txtAddress.Text.Length > 0)
+                {
+                    btnSave.Focus();
+                }
+                else
+                {
+                    txtAddress.Focus();
+                }
+            }
         }
     }
 }
